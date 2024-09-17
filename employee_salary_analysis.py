2f8f758d85e2b50dcc44d45d0443ebc78732785e
JARUGU SUPRIYA <142426723+JaruguSupriya@users.noreply.github.com>
2024-09-17 11:06:28 +0530

    Add files via upload

---
+++ employee_salary_analysis.py
@@ -0,0 +1,14 @@
+emp = {'ename': ['hari", "Thomas', 'Raj', 'Laksh', 'subha'], 'age': [21, 23, 22, 35, 28], 'salary': [15000, 18000, 26000, 30000, 10000]}
+print("The Employee datase is:", emp)
+print("The Employee salaries are:")
+print(emp['salary'])
+print("The salaries are:")
+for v in emp['salary']:
+    print(v)
+L1 = emp['salary']
+print("The listed salaries:", L1)
+print("The sorted salarles are:", sorted(L1))
+print("The reveresed salaries:",L1[::-1])
+print("The minimum salary is:", min(L1))
+print("The maximum salary is:", max(L1))
+print("The total salaary is:", sum(L1))
